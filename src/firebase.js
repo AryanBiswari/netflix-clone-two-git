93bef0f27be887392a558c82bc409b7720fa7a8a
Aryan Biswari <aryanbiswari14@gmail.com>
2021-08-08 17:30:43 +0530

    third change

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARy-jp0byIEREiJ7lpn_8u1ecY2ISVAgM",
+    authDomain: "moviesok-two.firebaseapp.com",
+    projectId: "moviesok-two",
+    storageBucket: "moviesok-two.appspot.com",
+    messagingSenderId: "242818649667",
+    appId: "1:242818649667:web:10b0ed063144bcf0be0ef6"
+  };
