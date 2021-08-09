006dc77178a576f86ff1c11e9ad7a96d7740b8ba
Aryan Biswari <aryanbiswari14@gmail.com>
2021-08-09 19:02:49 +0530

    sixth change

--- src/Nav.js
+++ src/Nav.js
@@ -2,8 +2,11 @@ import React from 'react';
 import "./Nav.css";
 function Nav() {
     return (
-        <div className="nav">
-            <h1>this is nav bar</h1>
+        <div className="nav nav__black">
+            <div className="nav__contents">
+                <img className="nav__logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
+                <img className="nav__avatar" src="https://iconape.com/wp-content/png_logo_vector/avatar-4.png" alt=""/>
+            </div>
         </div>
     )
 }
