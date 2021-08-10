c940d909fa88aa9dd39fac3690bada174fe32405
Aryan Biswari <aryanbiswari14@gmail.com>
2021-08-10 10:05:14 +0530

    nav bar complete

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState ,useEffect} from 'react';
 import "./Nav.css";
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if(window.scrollY > 100){
+            handleShow(true);
+        }
+        else{
+            handleShow(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, [])
     return (
-        <div className="nav nav__black">
+        <div className={`nav ${show && 'nav__black'}`}>
             <div className="nav__contents">
                 <img className="nav__logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
                 <img className="nav__avatar" src="https://iconape.com/wp-content/png_logo_vector/avatar-4.png" alt=""/>
@@ -12,3 +27,5 @@ function Nav() {
 }
 
 export default Nav
+
+/*resume at 59.09 */
